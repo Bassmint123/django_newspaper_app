396ee5f8ab03577e10abf531b1ea4b17c0ccd9a2
Bassmint123 <bradb@cimstrat.com>
2019-07-05 11:50:39 -0500

    Added Bootstrap frontend

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig', # Our users app customer user model
+    'pages.apps.PagesConfig', # Our pages app
 ]
 
 MIDDLEWARE = [
