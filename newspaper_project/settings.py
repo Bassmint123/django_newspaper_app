51d4540b4208db7cf7081664b07badc3fb27ac02
Bassmint123 <bradb@cimstrat.com>
2019-07-08 22:21:57 -0500

    Added URLs & Views for articles

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # Local
     'users.apps.UsersConfig', # Our users app customer user model
     'pages.apps.PagesConfig', # Our pages app
+    'articles.apps.ArticlesConfig', # Our articles app
 ]
 
 MIDDLEWARE = [
@@ -112,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Chicago'
 
 USE_I18N = True
 
