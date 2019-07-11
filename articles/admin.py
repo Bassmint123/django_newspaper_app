87e04771bde7140d38a8b3d70f2bcf3f614ff5d7
Bassmint123 <bradb@cimstrat.com>
2019-07-10 22:00:43 -0500

    Add the ability to make comment on an article from the admin

--- articles/admin.py
+++ articles/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Article
+from .models import Article, Comment
 
-admin.site.register(Article)
+"""
+We could display this with either TabularInline or StackedInline.
+
+class CommentInline(admin.TabularInline): # Tabular
+    model = Comment
+"""
+
+class CommentInline(admin.StackedInline): # Inline
+    model = Comment
+    """
+    By default, the Django admin will display 3 empty rows here. You can change the 
+    default number that appear with the extra field.
+    """
+    extra = 0 # new
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
