823f47e5a5ab16bc73480a0691e5db28d2828c76
Bassmint123 <bradb@cimstrat.com>
2019-07-12 16:31:48 -0500

    Added the ability to see comments on each article

--- articles/models.py
+++ articles/models.py
@@ -22,7 +22,9 @@ class Article(models.Model):
         return reverse('article_detail', args=[str(self.id)])
 
 class Comment(models.Model):
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+	# The related_name attribute is added to our model which lets us explicitly set the name of the 
+	# reverse relationship in working with the foreign key.
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments',)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         get_user_model(),
